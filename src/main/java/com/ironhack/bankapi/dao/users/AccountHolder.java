7c0d30797ce6a0a38af71fca4140e0212e4cf51d
Gustavo Maldonado <maldonadogustavo55@gmail.com>
2021-09-28 11:48:29 +0200

    Add Address class and add to AccountHolder

--- src/main/java/com/ironhack/bankapi/dao/users/AccountHolder.java
+++ src/main/java/com/ironhack/bankapi/dao/users/AccountHolder.java
@@ -3,10 +3,12 @@ package com.ironhack.bankapi.dao.users;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ironhack.bankapi.dao.accounts.Account;
+import com.ironhack.bankapi.utils.Address;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -22,13 +24,31 @@ public class AccountHolder extends User {
 
     private LocalDate dateOfBirth;
 
-    private String address; // Should be a separate Address class
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "street", column = @Column(nullable = false)),
+            @AttributeOverride(name = "number", column = @Column(name = "street_number", nullable = false)),
+            @AttributeOverride(name = "state", column = @Column(nullable = false)),
+            @AttributeOverride(name = "country", column = @Column(nullable = false)),
+            @AttributeOverride(name = "zipCode", column = @Column(name = "zip_code", nullable = false))
+    })
+    private Address address; // Should be a separate Address class
 
-    private String mailingAddress; // Should be optional
+    @Embedded
+    @Nullable
+    @AttributeOverrides({
+            @AttributeOverride(name = "street", column = @Column(name = "mailing_street", nullable = false)),
+            @AttributeOverride(name = "number", column = @Column(name = "mailing_street_number", nullable = false)),
+            @AttributeOverride(name = "state", column = @Column(name = "mailing_state",nullable = false)),
+            @AttributeOverride(name = "country", column = @Column(name = "mailing_country",nullable = false)),
+            @AttributeOverride(name = "zipCode", column = @Column(name = "mailing_zip_code", nullable = false))
+    })
+    private Address mailingAddress; // Should be optional
 
     @OneToMany(mappedBy = "primaryOwner", cascade = CascadeType.ALL)
     private List<Account> accountsPrimary;
 
+    @Nullable
     @OneToMany(mappedBy = "secondaryOwner", cascade = CascadeType.ALL)
     private List<Account> accountsSecondary;
 
--- src/main/java/com/ironhack/bankapi/utils/Address.java
+++ src/main/java/com/ironhack/bankapi/utils/Address.java
@@ -1,2 +1,18 @@
-package com.ironhack.bankapi.utils;public class Address {
+package com.ironhack.bankapi.utils;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Data
+@NoArgsConstructor(force = true)
+public class Address {
+
+    private String street;
+    private String number;
+    private String state;
+    private String country;
+    private String zipCode;
 }
